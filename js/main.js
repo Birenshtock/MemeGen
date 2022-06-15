619c36045df7941f93ca4ffc9380f80ce22bdf79
Birenshtock <106261139+Birenshtock@users.noreply.github.com>
2022-06-15 19:56:04 +0300

    first submition

--- js/main.js
+++ js/main.js
@@ -3,6 +3,25 @@
 
 var gCanvas;
 var gCtx;
+var gCurrText
+var gFillColor
+var gStrokeColor
+var gX
+var gY
+var gTextNum = 0
+var gCurrFont
+
+var enterEvent = document.querySelector('.item0')
+enterEvent.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+        // code for enter
+        enterEvent.value = ''
+        gTextNum++
+        enterEvent.blur() //עושה שלא אוכל לכתוב שוב באינפוט אחרי הפעם הראשונה
+        console.log('gTextNum:', gTextNum)
+    }
+})
+
 
 function init() {
     gCanvas = document.querySelector('#my-canvas');
@@ -19,3 +38,72 @@ function drawImgFromlocal() {
         gCtx.drawImage(img, 0, 0, gCanvas.width, gCanvas.height) //img,x,y,xend,yend
     }
 }
+
+
+function drawText(text, x = 100, y = 100) {
+    gCtx.lineWidth = 2;
+    gCtx.strokeStyle = gStrokeColor;
+    gCtx.fillStyle = gFillColor;
+    gCtx.font = `40px ${gCurrFont}`;
+    // gCanvas.style.fontSize = "400px";
+    gCtx.fillText(text, x, y); //Draws (fills) a given text at the given (x, y) position.
+    gCtx.strokeText(text, x, y); //Draws (strokes) a given text at the given (x, y) position.
+}
+
+function onSetFont(font) {
+    gCurrFont = font
+}
+
+function onBiggerFont() {
+    gCtx.font = gCtx.font.replace(/\d+px/, (parseInt(gCtx.font.match(/\d+px/)) + 2) + "px");
+
+    console.log('size:', gCtx.font)
+
+}
+
+
+
+function OnSetFillColor(FillColor) {
+    gFillColor = FillColor
+    console.log(gFillColor)
+}
+
+function OnSetStrokeColor(StrokeColor) {
+    gStrokeColor = StrokeColor
+    console.log(gStrokeColor)
+}
+
+function onTextRight() {
+    gCtx.textAlign = "end"
+    gX = 550
+}
+
+function onTextLeft() {
+    gCtx.textAlign = "start"
+    gX = 100
+}
+
+function onTextCenter() {
+    gCtx.textAlign = "center";
+    gX = gCanvas.width / 2
+}
+
+function onPlusAnotherText() {
+
+    if (gTextNum === 1) {
+        gY = 500
+    } else if (gTextNum >= 2) {
+        gY = 300
+    }
+}
+
+function onAddTxt(text) { //הטקסט שהיוזר כותב
+    console.log(text)
+    gCurrText = text
+    drawText(gCurrText, gX, gY)
+}
+
+// function onDeleteText() {
+
+
+// }
